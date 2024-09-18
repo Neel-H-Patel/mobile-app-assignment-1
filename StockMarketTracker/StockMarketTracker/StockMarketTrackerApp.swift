f134877e42f248a5b46aad63335d4ec837868d3a
Neel Patel <neelpatel@smu.edu>
2024-09-18 16:53:37 -0500

    added base utilities

---
+++ StockMarketTracker/StockMarketTracker/StockMarketTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockMarketTrackerApp.swift
+//  StockMarketTracker
+//
+//  Created by Neel Patel on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct StockMarketTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
